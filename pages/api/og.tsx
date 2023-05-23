bb08d55cc064d8005dc2295b85065ffbf489f5d1
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-05-23 18:40:37 +0300

    Add

---
+++ pages/api/og.tsx
@@ -0,0 +1,65 @@
+import { ImageResponse } from "@vercel/og";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default function og() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          height: "100%",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          backgroundImage: "linear-gradient(to bottom, #dbf4ff, #fff1f1)",
+          fontSize: 60,
+          letterSpacing: -2,
+          fontWeight: 700,
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            backgroundImage:
+              "linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))",
+            backgroundClip: "text",
+            "-webkit-background-clip": "text",
+            color: "transparent",
+          }}
+        >
+          Handle
+        </div>
+        <div
+          style={{
+            backgroundImage:
+              "linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))",
+            backgroundClip: "text",
+            "-webkit-background-clip": "text",
+            color: "transparent",
+          }}
+        >
+          100k
+        </div>
+        <div
+          style={{
+            backgroundImage:
+              "linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))",
+            backgroundClip: "text",
+            "-webkit-background-clip": "text",
+            color: "transparent",
+          }}
+        >
+          Products
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    }
+  );
+}
